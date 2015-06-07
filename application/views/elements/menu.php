187b9ae06dc41b6d6c1698dffc258340bde78419
Amit <amitg@beckett.com>
2015-06-07 17:00:53 +0530

    update

--- application/views/elements/menu.php
+++ application/views/elements/menu.php
@@ -8,9 +8,7 @@ if (trim($data['user_type']) == 'admin'):
         <li>
             <a href="#">&nbsp;Admin</a>
             <ul>
-                <li><a href="<?php echo base_url() ?>site/testsRequest" >Take Tests Request </a></li>
-                <li><a href="<?php echo base_url() ?>site/getOrders" >Update/Delete Tests Request</a></li>
-                <li><a href="#">Add Lab User</a></li>
+                <li><a href="<?php echo base_url() ?>users/createuser" >Create User</a></li>
             </ul>
         </li>
         <li><a href="#">&nbsp;Reports</a>
@@ -19,6 +17,40 @@ if (trim($data['user_type']) == 'admin'):
                 <li><a href="#" >View & send Reports</a></li>
             </ul>
         </li>
+        <li>
+            <a href="<?php echo base_url() ?>login/logout">
+                Logout
+            </a>
+        </li>
+    </ul>
+<?php elseif (trim($data['user_type']) == 'rto_staff'): ?>
+    <ul class="PaycMenu">
+        <li>
+            <a href="<?php echo base_url() ?>vehicles/index">My Vehicles</a>
+            <ul>
+                <li><a href="<?php echo base_url() ?>vehicles/add" >Add New Vehicle</a></li>
+                <li><a href="<?php echo base_url() ?>vehicles/index" >Vehicle List</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="<?php echo base_url() ?>users/profile">My Profile</a>
+        <li>
+            <a href="<?php echo base_url() ?>login/logout">
+                Logout
+            </a>
+        </li>
+    </ul>
+<?php elseif (trim($data['user_type']) == 'pol_staff'): ?>
+    <ul class="PaycMenu">
+        <li>
+            <a href="<?php echo base_url() ?>vehicles/index">My Vehicles</a>
+            <ul>
+                <li><a href="<?php echo base_url() ?>vehicles/add" >Add New Vehicle</a></li>
+                <li><a href="<?php echo base_url() ?>vehicles/index" >Vehicle List</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="<?php echo base_url() ?>users/profile">My Profile</a>
         <li>
             <a href="<?php echo base_url() ?>login/logout">
                 Logout
